2d8d0ed576dd23268c597f258edad528b913d73e
T-death <suketawada0612@gmail.com>
2024-07-20 21:26:32 +0900

    [add] Profilerを使った描画の計測をやった

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ThrowResult from './chap5/ThrowResult';
+import ProfilerBasic from './chap5/ProfilerBasic';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThrowResult />
+  <ProfilerBasic />
 );

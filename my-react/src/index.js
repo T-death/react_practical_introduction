bd8f6bf00ff2e7f9ccaaa7cc3a6229e8b1c51f97
T-death <suketawada0612@gmail.com>
2024-07-27 11:50:26 +0900

    [add] useContextの使い方をやった

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,24 +1,11 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ErrorBoundary } from 'react-error-boundary';
-import QuerySuspense from './chap6/QuerySuspence';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import MyThemeProvider from './chap7/MyThemeProvider';
+import HookThemeButton from './chap7/HookThemeButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const cli = new QueryClient({
-  defaultOptions: {
-    queries: {
-      suspense: true,
-    }
-  }
-});
-
 root.render(
-  <Suspense fallback={<p>Loading...</p>}>
-    <ErrorBoundary fallback={<div>エラーが発生しました。</div>}>
-      <QueryClientProvider client={cli}>
-        <QuerySuspense />
-      </QueryClientProvider>
-    </ErrorBoundary>
-  </Suspense>
+  <MyThemeProvider>
+    <HookThemeButton />
+  </MyThemeProvider>
 );
